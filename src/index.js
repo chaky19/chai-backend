1e6a0d4179453b55eadc52c5b88e5af6cd4771b6
Bittu <chaitanya.pandey007@gmail.com>
2023-12-23 19:12:14 +0530

    databse setup

---
+++ src/index.js
@@ -0,0 +1,20 @@
+// require('dotenv').config({path: './env'});
+
+import dotenv from 'dotenv';
+import connectDB from "./db/index.js"
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB();
+
+
+// (async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MANGODB_URI}/${DB_NAME}`)
+//     } catch (error) {
+//         console.log("Error: ", error);
+//         throw error
+//     }
+// })()
